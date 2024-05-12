17ec95fddae070cc4913fe6d33551134f4d73456
ulofreitas <83433693+ulofreitas@users.noreply.github.com>
2024-05-11 18:02:26 -0700

    added more tests

--- jewels_in_stones.py
+++ jewels_in_stones.py
@@ -5,16 +5,26 @@ You want to know how many of the stones you have are also jewels.
 
 Letters are case sensitive, so "a" is considered a different type of stone from "A".
 
-Example 1:
 
+Example 1:
 Input: jewels = "aA", stones = "aAAbbbb"
 Output: 3
-Example 2:
 
-Input: jewels = "z", stones = "ZZ"
+Example 2:
+Input: jewels = "abc", stones = "ABC"
 Output: 0
 
+Example 3:
+Input: jewels = "AbC", stones = "ABC"
+Output: 2
+
+Example 4:
 Input: jewels = "py", stones = "Python is a language, but python is also a snake!"
+Output: 3
+
+Example 5:
+Input: jewels = "sTrInG", stones = "STRINGS are really cool!"
+Output: 5
 """
 
 def numJewelsInStones(jewels, stones):
@@ -31,7 +41,11 @@ def numJewelsInStones(jewels, stones):
 
 # Testing
 assert(numJewelsInStones(jewels="aA", stones="aAAbbbb") == 3)
-assert(numJewelsInStones(jewels="z", stones="ZZ") == 0)
+assert(numJewelsInStones(jewels="abc", stones="ABC") == 0)
+assert(numJewelsInStones(jewels="AbC", stones="ABC") == 2)
 assert(numJewelsInStones(jewels="py", stones="Python is a language, but python is also a snake!") == 3)
+assert(numJewelsInStones(jewels="sTrInG", stones="STRINGS are really cool!") == 5)
 # TODO: add more tests!
+
+# If you see this message, that means your code works :D
 print("✅✅ All tested passed ✅✅")
